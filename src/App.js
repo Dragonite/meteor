b12797bdce83a8eb892fdf375cd4e2c9bc64100c
Dragonite <haolin@192-168-1-9.tpgi.com.au>
2018-12-30 17:52:20 +0800

    Modified component to PureComponent

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import logo from './snowflake.svg';
 import githubwhite from './githubwhite.png';
 import linkedin from './linkedin.png';
 import instagram from './instagram.png';
 import './App.css';
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          
+          <img src={logo} className="App-logo" alt="logo" />    
         </div>
-
         <div className="App-body">
           <a href="https://github.com/Dragonite"><img src={githubwhite}/></a>
           <a href="https://www.linkedin.com/in/haolin-wu-222188160/"><img src={linkedin}/></a>
@@ -24,5 +22,4 @@ class App extends Component {
     );
   }
 }
-
 export default App;
